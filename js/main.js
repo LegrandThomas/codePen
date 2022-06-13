cc24b2c0e1247d56c53de24a24fb5faa854add90
Legrand Thomas <pro.legrand.thomas@gmail.com>
2022-06-13 16:52:46 +0200

    compteur ok sans balise

--- js/main.js
+++ js/main.js
@@ -3,8 +3,34 @@ function affiche(){
     let css="<style>"+document.getElementById("CSS").value+"</style>";
     let js="<script>"+document.getElementById("JS").value+"</script>";
     let code=document.getElementById("code").contentWindow.document;
+    let compteur=document.getElementById("code").innerHTML;
+    let test = html.replace(/(<([^>]+)>)/gi, "");
 
      code.open();
      code.write(html+css+js);
+     compteur=html.length;
+     //console.log(compteur);
      code.close();
-}
+     //console.log(test);
+   }
+
+     document.getElementById('HTML').addEventListener('keyup', function() {
+        let y=document.getElementById("y").innerHTML;
+        y=document.getElementById("HTML").value.replace(/(<([^>]+)>)/gi, "");
+        document.getElementById('compteur').innerHTML =y.length;
+        console.log(y.length);
+    });
+  
+   
+  
+  
+
+
+
+
+
+
+
+
+
+
