0c730949afd66323beb29c3b7d5b04592d3b2d0c
Legrand Thomas <pro.legrand.thomas@gmail.com>
2022-06-14 15:36:02 +0200

    +bonus

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ function affiche(){
     let code=document.getElementById("code").contentWindow.document;
     let compteur=document.getElementById("code").innerHTML;
     let test = html.replace(/(<([^>]+)>)/gi, "");
+    
+
 
      code.open();
      code.write(html+css+js);
@@ -12,22 +14,60 @@ function affiche(){
      //console.log(compteur);
      code.close();
      //console.log(test);
+
+   
    }
 
      document.getElementById('HTML').addEventListener('keyup', function() {
         let y=document.getElementById("y").innerHTML;
+        let progressBar=document.getElementById("progressBar");
+        
+        
+
         y=document.getElementById("HTML").value.replace(/(<([^>]+)>)/gi, "");
         document.getElementById('compteur').innerHTML =y.length;
-        console.log(y.length);
+        let num= y.length/2;
+        progressBar.value=num;
+   
+       
+    
+        // console.log(progressBar);
+        // console.log(y.length);
+    });
+
+    let baliseB=document.getElementById('b').addEventListener('click',function(){
+      let html=document.getElementById("HTML").value+="<b></b>";
     });
   
+    let baliseh=document.getElementById('h').addEventListener('click',function(){
+      let html=document.getElementById("HTML").value+="<h1></h1>";
+    });
+  
+    let balisea=document.getElementById('a').addEventListener('click',function(){
+      let html=document.getElementById("HTML").value+="<a></a>";
+    });
    
+    let f=document.getElementById('section').addEventListener('click',function(){
+      let spinner=document.getElementById("infinity-loader");
+         spinner.className = "test";
+        // console.log(spinner.className);
+    });
+
+
+    const boxes = document.querySelectorAll('textarea');
+
+        boxes.forEach(box => {
+        box.addEventListener('click', function handleClick(event) {
+        let spinner=document.getElementById("infinity-loader");
+        spinner.className = "infinity-loader";
+      
+
+    
+  });
+});
+
   
-  
-
-
-
-
+   
 
 
 
